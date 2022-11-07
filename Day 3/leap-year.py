94d7bbca411a1555a5a81cc6c95ea9c0ed960e95
Aro Arko <aroarko.sd@gmail.com>
2022-11-07 20:35:34 +0800

    leap-year

---
+++ Day 3/leap-year.py
@@ -0,0 +1,18 @@
+# 🚨 Don't change the code below 👇
+year = int(input("Which year do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+n_year = str(year)
+last_value = int(n_year[3])
+second_last = int(n_year[2])
+
+# print(last_value)
+# print(second_last)
+
+if (((last_value and second_last) == 0) and (year % 4 == 0) and (year % 100 == 0) and (year % 400 == 0)):
+    print("Leap year")
+elif(((last_value or second_last) != 0) and (year % 4 == 0)):
+    print("Leap year")
+else:
+    print("Not Leap year")
---
+++ Day 3/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+last_value = int(n_year[3])
+# second_last = int(n_year[2])
+
+# print(last_value)
+# print(second_last)
