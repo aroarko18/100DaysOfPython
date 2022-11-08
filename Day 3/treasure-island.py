27f0b3996a8a6824f23b18c8580bc83fe1e7e112
Aro Arko <aroarko.sd@gmail.com>
2022-11-08 21:51:54 +0800

    treasure-island input lower

--- Day 3/treasure-island.py
+++ Day 3/treasure-island.py
@@ -27,12 +27,12 @@ print("Your mission is to find the treasure.")
 
 #Write your code below this line 👇
 
-cross_road = input('You' + "'" + 're at a cross road. Where do you want to go? Type "left" or "right"\n')
+cross_road = input('You' + "'" + 're at a cross road. Where do you want to go? Type "left" or "right"\n').lower()
 
 if cross_road == "left":
-    wait_swim = input('You' + "'ve" + ' come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across.\n')
+    wait_swim = input('You' + "'ve" + ' come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across.\n').lower()
     if wait_swim == "wait":
-        door = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?\n")
+        door = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?\n").lower()
         if door == "red":
             print("It's a room full of fire. Game Over.")
         elif door == "yellow":
