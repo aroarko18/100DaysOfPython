fb023c65b7bc2c893dcf75b1cf93d6a5c916a80d
Aro Arko <aroarko.sd@gmail.com>
2022-11-09 21:58:55 +0800

    treasure-map row colum all done

---
+++ Day 4/treasure-map.py
@@ -0,0 +1,46 @@
+# 🚨 Don't change the code below 👇
+row1 = ["⬜️","️⬜️","️⬜️"]
+row2 = ["⬜️","⬜️","️⬜️"]
+row3 = ["⬜️️","⬜️️","⬜️️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+# reversed_position = position[::-1]
+# print(len(reversed_position))
+
+# print(position[1] + position[0])
+# row 1
+if position[1] == "1":
+    if position[0] == "1":
+        row1[0] = 'X'
+    elif position[0] == "2":
+        row1[1] = 'X'
+    elif position[0] == "3":
+        row1[2] = 'X'
+
+# row 2
+if position[1] == "2":
+    if position[0] == "1":
+        row2[0] = 'X'
+    elif position[0] == "2":
+        row2[1] = 'X'
+    elif position[0] == "3":
+        row2[2] = 'X'
+
+# row 3
+if position[1] == "3":
+    if position[0] == "1":
+        row3[0] = 'X'
+    elif position[0] == "2":
+        row3[1] = 'X'
+    elif position[0] == "3":
+        row3[2] = 'X'
+
+#Write your code above this row 👆
+
+# 🚨 Don't change the code below 👇
+print(f"{row1}\n{row2}\n{row3}")
+
