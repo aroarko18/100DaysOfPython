a9624ab5c0e314f505fe5d89a43ff3f54307217f
Aro Arko <aroarko.sd@gmail.com>
2022-11-10 00:08:37 +0800

    treasure-map another solution

--- Day 4/treasure-map.py
+++ Day 4/treasure-map.py
@@ -8,36 +8,45 @@ position = input("Where do you want to put the treasure? ")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this row 👇
+
+horizontal = int(position[0])
+vertical = int(position[1])
+
+selected_row = map[vertical - 1]
+selected_row[horizontal - 1] = "X"
+
+
+###my solution
 # reversed_position = position[::-1]
 # print(len(reversed_position))
 
 # print(position[1] + position[0])
 # row 1
-if position[1] == "1":
-    if position[0] == "1":
-        row1[0] = 'X'
-    elif position[0] == "2":
-        row1[1] = 'X'
-    elif position[0] == "3":
-        row1[2] = 'X'
+# if position[1] == "1":
+#     if position[0] == "1":
+#         row1[0] = 'X'
+#     elif position[0] == "2":
+#         row1[1] = 'X'
+#     elif position[0] == "3":
+#         row1[2] = 'X'
 
-# row 2
-if position[1] == "2":
-    if position[0] == "1":
-        row2[0] = 'X'
-    elif position[0] == "2":
-        row2[1] = 'X'
-    elif position[0] == "3":
-        row2[2] = 'X'
+# # row 2
+# if position[1] == "2":
+#     if position[0] == "1":
+#         row2[0] = 'X'
+#     elif position[0] == "2":
+#         row2[1] = 'X'
+#     elif position[0] == "3":
+#         row2[2] = 'X'
 
-# row 3
-if position[1] == "3":
-    if position[0] == "1":
-        row3[0] = 'X'
-    elif position[0] == "2":
-        row3[1] = 'X'
-    elif position[0] == "3":
-        row3[2] = 'X'
+# # row 3
+# if position[1] == "3":
+#     if position[0] == "1":
+#         row3[0] = 'X'
+#     elif position[0] == "2":
+#         row3[1] = 'X'
+#     elif position[0] == "3":
+#         row3[2] = 'X'
 
 #Write your code above this row 👆
 
