7e809dc76ad99440bf1c01b85af60af123e942cd
Aro Arko <aroarko.sd@gmail.com>
2022-11-18 04:21:32 +0800

    hurdle 4 bug solved

--- Day 6/Hurdle 4.py
+++ Day 6/Hurdle 4.py
@@ -1,30 +1,52 @@
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+# def jump():
+#     if wall_in_front():
+#         turn_left()
+#         if wall_in_front():
+#             turn_left()
+#             move()
+#         else:
+#             move()
+#     elif wall_on_right():
+#         turn_right()
+#         turn_left()
+#         if wall_in_front():
+#             turn_left()
+#             if wall_in_front():
+#                 turn_left()
+#                 move()
+#         else:
+#             move()
+#     elif right_is_clear():
+#         turn_right()
+#         move()
+# while at_goal() != True:
+#     jump()
+
+
 def turn_right():
     turn_left()
     turn_left()
     turn_left()
 def jump():
-    if wall_in_front():
-        turn_left()
-        if wall_in_front():
-            turn_left()
-            move()
-        else:
-            move()
-    elif wall_on_right():
-        turn_right()
-        turn_left()
-        if wall_in_front():
-            turn_left()
-            if wall_in_front():
-                turn_left()
-                move()
-        else:
-            move()
-    elif right_is_clear():
-        turn_right()
+    turn_left()
+    while wall_on_right():
         move()
+    turn_right()
+    move()
+    turn_right()
+    while front_is_clear():
+        move()
+    turn_left()
+
 while at_goal() != True:
-    jump()
+    if wall_in_front():
+        jump()
+    else:
+        move()
 ################################################################
 # WARNING: Do not change this comment.
 # Library Code is below.
