489ada6e90d825f97870c0d11dc06330550c41b4
Aro Arko <aroarko.sd@gmail.com>
2022-12-06 20:34:13 +0800

    decoded

--- Day 8/caesar-cipher.py
+++ Day 8/caesar-cipher.py
@@ -1,37 +1,32 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 def encrypt(message = text, difference = shift):
-
-    
-    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
     encrypt_text = []
     for letter in message:
         position = alphabet.index(letter)
         position = position + difference
-        if position > 26:
-            new_position = position - 26
-            encrypt_message = alphabet[new_position]
-        else:
-            encrypt_message = alphabet[position]
+        
+        encrypt_message = alphabet[position]
         encrypt_text.append(encrypt_message)    
     print("The encoded text is " + ''.join(encrypt_text))
-    #e.g. 
-    #plain_text = "hello"
-    #shift = 5
-    #cipher_text = "mjqqt"
-    #print output: "The encoded text is mjqqt"
 
-    ##HINT: How do you get the index of an item in a list:
-    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
+def decrypt(message = text, difference = shift):
+    decrypt_text = ''
+    for letter in message:
+        position = alphabet.index(letter)
+        position = position - difference
 
-    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
+        decrypt_message = alphabet[position]
+        decrypt_text = decrypt_text + decrypt_message
+    print(f"The decoded text is {decrypt_text}")
 
-
-
-#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
-encrypt()
+if direction == "encode":
+    encrypt()
+elif direction == "decode":
+    decrypt()
+else:
+    print("Sorry! Your direction is wrong.")
