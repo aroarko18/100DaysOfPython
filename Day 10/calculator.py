ab00ade84b2b4719f87bc31d4c824e653396a7ed
Aro Arko <aroarko.sd@gmail.com>
2022-12-10 02:59:00 +0800

    calculator

--- Day 10/calculator.py
+++ Day 10/calculator.py
@@ -31,5 +31,10 @@ for operation in operations:
 operation_symbol = input("Pick an operation from the line above: ")
 
 calculation_function = operations[operation_symbol]
-answer = calculation_function(number1, number2)
-print(f"{number1} {operation_symbol} {number2} = {answer}")
+first_answer = calculation_function(number1, number2)
+print(f"{number1} {operation_symbol} {number2} = {first_answer}")
+
+number3 = int(input("Enter another number: "))
+operation_symbol = input("Pick an operation from the line above: ")
+second_answer = calculation_function(calculation_function(number1, number2), number3)
+print(f"{first_answer} {operation_symbol} {number3} = {second_answer}")
