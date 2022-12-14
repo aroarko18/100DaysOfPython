a25b0c5d30cc76872cb94be0a16fd9645a0adb75
Aro Arko <aroarko.sd@gmail.com>
2022-12-15 02:30:59 +0800

    calculator done

--- Day 10/calculator.py
+++ Day 10/calculator.py
@@ -1,4 +1,21 @@
 # Calculator
+logo = """
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+print(logo)
 
 # Add
 def add(num1, num2):
@@ -23,18 +40,25 @@ operations = {
     "/": divide,
 }
 
-number1 = int(input("Enter your first number: "))
-number2 = int(input("Enter your second number: "))
+def calculator():
+    number1 = float(input("Enter your first number: "))
 
-for operation in operations:
-    print(operation)
-operation_symbol = input("Pick an operation from the line above: ")
+    for operation in operations:
+        print(operation)
 
-calculation_function = operations[operation_symbol]
-first_answer = calculation_function(number1, number2)
-print(f"{number1} {operation_symbol} {number2} = {first_answer}")
+    should_continue = True
 
-number3 = int(input("Enter another number: "))
-operation_symbol = input("Pick an operation from the line above: ")
-second_answer = calculation_function(calculation_function(number1, number2), number3)
-print(f"{first_answer} {operation_symbol} {number3} = {second_answer}")
+    while should_continue:
+        operation_symbol = input("Pick an operation from the line above: ")
+        number2 = float(input("Enter your another number: "))
+        calculation_function = operations[operation_symbol]
+        first_answer = calculation_function(number1, number2)
+        print(f"{number1} {operation_symbol} {number2} = {first_answer}")
+
+
+        if input(f"Type 'y' to continue with the {first_answer} or 'n' to exit- ") == "y":
+            number1 = first_answer
+        else:
+            should_continue = False
+            calculator()
+calculator()
