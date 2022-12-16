87f5ed1e861b7e3741b73c52e71bf093d27dde7b
Aro Arko <aroarko.sd@gmail.com>
2022-12-17 03:49:47 +0800

    guess-the-number

---
+++ Day_12/number-guess.py
@@ -0,0 +1,40 @@
+import random
+
+
+print("Welcome to the Number Guessing Game!\nI am thinking of a number between 1 and 100.")
+
+number = random.randint(1, 100)
+print(f"Pssst, the correct answer is {number}")
+
+difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
+
+attempts = 5
+
+if difficulty == "easy":
+    attempts = 10
+elif difficulty == "hard":
+    attempts == 5
+print(f"You have {attempts} attempts remaining to guess the number.")
+
+should_run = True
+while should_run:
+    guess = int(input("Make a guess: "))
+
+    if guess == number:
+        print(f"You got it! The answer was {number}")
+        should_run = False
+    elif guess > number and attempts > 0:
+        print("Too high.")
+        print("Guess again")
+        attempts -= 1
+    elif guess < number and attempts > 0:
+        print("Too low.")
+        print("Guess again")
+        attempts -= 1
+    if attempts >= 1:
+        print(f"You have {attempts} attempts remaining to guess the number.")
+    if attempts == 0:
+        print("You have run out of guesses, you lose.")
+        should_run = False
+    
+    
