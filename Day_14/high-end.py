4d9090c1b4477b354a600f8d37b5dd0ff9a91323
Aro Arko <aroarko.sd@gmail.com>
2023-01-04 15:19:52 +0800

    high-end done

--- Day_14/high-end.py
+++ Day_14/high-end.py
@@ -1,6 +1,7 @@
 import art
 from game_data import data
 import random
+import os
 
 
 #display art
@@ -19,27 +20,34 @@ def check_answer(guess, follower_a, follower_b):
     else:
         return guess == "b"
 
-#account
-account_a = random.choice(data)
+should_continue = True
 account_b = random.choice(data)
-if account_a == account_b:
-    account_b = random.choice(data)
-
-print(f"Compare A: {format_data(account_a)}")
-# art --- vs
-print(art.vs)
-print(f"Compare B: {format_data(account_b)}")
-
-guess = input("Who has more follower? ").lower()
-follower_count_a = account_a["follower_count"]
-follower_count_b = account_b["follower_count"]
-is_correct = check_answer(guess, follower_count_a, follower_count_b)
-# print(is_correct)
-
-
 score = 0
-if is_correct:
-    score += 1
-    print(f"You're right! Current score: {score}")
-else:
-    print(f"Sorry that's wrong. Final score: {score}")
+
+while should_continue:
+    #account
+    account_a = account_b
+    account_b = random.choice(data)
+    if account_a == account_b:
+        account_b = random.choice(data)
+
+    print(f"Compare A: {format_data(account_a)}")
+    # art --- vs
+    print(art.vs)
+    print(f"Against B: {format_data(account_b)}")
+
+
+    guess = input("Who has more follower? ").lower()
+    follower_count_a = account_a["follower_count"]
+    follower_count_b = account_b["follower_count"]
+    is_correct = check_answer(guess, follower_count_a, follower_count_b)
+    # print(is_correct)
+    os.system('cls')
+    print(art.logo)
+
+    if is_correct:
+        score = score + 1
+        print(f"You're right! Current score: {score}")
+    else:
+        print(f"Sorry that's wrong. Final score: {score}")
+        should_continue = False
--- Day_14/tempCodeRunnerFile.py
+++ Day_14/tempCodeRunnerFile.py
@@ -1,1 +1,2 @@
-name
+
+    print(art.logo)
